a48b5de2f41ea4977dd064ae78cb9b99ecf3c50c
Jordan B. Hansen <jbhansen84@gmail.com>
2014-03-20 10:17:05 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dota2caster::Application.config.secret_key_base = '142d62765ecf4d1bb3a020b1a8c15dbba06c44e3c4e96a8b461ee5ca60e87b57e8ef059afa701a38a035bcbe7b3a297e876325aa6a5b3aef4d455e6d29721747'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dota2caster::Application.config.session_store :cookie_store, key: '_dota2caster_session'
