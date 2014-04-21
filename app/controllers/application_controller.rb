76e4d52ded13d477f79c9d0d0d4d01dc62cccdc1
Jordan Hansen <jbhansen84@gmail.com>
2014-04-20 20:36:12 -0600

    Update each time you log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #before_filter :load_user
+  before_filter :load_user
   before_filter :check_active_streams
  	protect_from_forgery with: :null_session
 
+
+  def load_user
+    # In rails 4, this would be .find_or_create_by! (:steamid => session[:current_user][:uid])
+    @current_user = Caster.find_or_create_by! (:steamid => session[:current_user][:uid])
+    # If they are a new user, ship them over to the profile page
+    if @current_user && @current_user.new_record?
+      @current_user.save!
+      redirect_to root_path
+    elsif @current_user
+      Caster.find(@current_user.id).update_attributes(:name => session[:current_user][:nickname])
+    end
+
+    @can_edit = @current_user && @current_user.is_admin?
+  end
 
   def check_active_streams
   	@twitch_accounts = ["amateurdota2league","amateurdota2league1", "bravenw_dota", "5midasgaming"]
