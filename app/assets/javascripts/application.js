981d3910905aa8f5b441a1047f420fd8a2721a26
Jordan Hansen <jbhansen84@gmail.com>
2014-05-28 07:06:53 -0600

    full screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46030946-2', 'dota2caster.com');
+ga('create', 'UA-46030946-3', 'castdota.com');
 ga('send', 'pageview');
 
 $(document).ready(function(){
